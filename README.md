6b3deb9e244b25a40b9ae273043d71d966fb1623
jhg83422 <jhg8342@gachon.ac.kr>
2023-10-26 17:21:23 +0900

    test 123123

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2023_ETRI_openAPI_team_cheer
-2023 ETRI 오픈 API 공모전 팀 cheer
---
+++ app/src/main/java/com/example/a2023_etri_openapi_team_cheer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2023_etri_openapi_team_cheer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
